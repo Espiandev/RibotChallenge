ad298e54ee263eb075c178596eb71e3bdbf0a4cf
Alex Curran <aml.curran@gmail.com>
2013-04-28 13:39:53 +0100

    Orientation-sensing code for the StudioImageLoader - although doesn't seem to work

--- src/com/espian/ribotchallenge/loaders/StudioImageLoader.java
+++ src/com/espian/ribotchallenge/loaders/StudioImageLoader.java
@@ -4,6 +4,8 @@ import android.content.AsyncTaskLoader;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+import android.media.ExifInterface;
 
 import java.io.*;
 import java.net.HttpURLConnection;
@@ -34,7 +36,12 @@ public class StudioImageLoader extends AsyncTaskLoader<List<Bitmap>> implements 
 			File[] files = getContext().getCacheDir().listFiles(this);
 			List<Bitmap> fileBitmaps = new ArrayList<Bitmap>();
 			for (File file : files) {
-				fileBitmaps.add(BitmapFactory.decodeFile(file.getAbsolutePath()));
+				try {
+					Bitmap start = BitmapFactory.decodeFile(file.getAbsolutePath());
+					fileBitmaps.add(rotateBitmap(start, new ExifInterface(file.getAbsolutePath())));
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
 			}
 			return fileBitmaps;
 		}
@@ -49,6 +56,10 @@ public class StudioImageLoader extends AsyncTaskLoader<List<Bitmap>> implements 
 
 				connection = (HttpURLConnection) new URL(url).openConnection();
 				Bitmap result = BitmapFactory.decodeStream(new BufferedInputStream(connection.getInputStream()));
+
+				// Check image rotation
+				result = rotateBitmap(result, new ExifInterface(url));
+
 				if (result != null) loadedBitmaps.add(result);
 
 				// Now, we want to write to the cache file
@@ -78,6 +89,38 @@ public class StudioImageLoader extends AsyncTaskLoader<List<Bitmap>> implements 
 
 		}
 		return loadedBitmaps;
+
+	}
+
+	public Bitmap rotateBitmap(Bitmap input, ExifInterface exifInterface) {
+
+		// This doesn't actually seem to work... Exif data doesn't seem to get obtained properly
+		int rotationDegree = 0;
+		switch (exifInterface.getAttributeInt(ExifInterface.TAG_ORIENTATION, ExifInterface.ORIENTATION_NORMAL)) {
+
+			case ExifInterface.ORIENTATION_NORMAL:
+				return input;
+
+			case ExifInterface.ORIENTATION_ROTATE_90:
+				rotationDegree = 90;
+				break;
+
+			case ExifInterface.ORIENTATION_ROTATE_180:
+				rotationDegree = 180;
+				break;
+
+			case ExifInterface.ORIENTATION_ROTATE_270:
+				rotationDegree = 270;
+				break;
+
+		}
+
+	    if (rotationDegree == 0) return input;
+		else {
+			Matrix matrix = new Matrix();
+			matrix.postRotate(rotationDegree);
+			return Bitmap.createBitmap(input, 0, 0, input.getWidth(), input.getHeight(), matrix, true);
+		}
 
 	}
 
